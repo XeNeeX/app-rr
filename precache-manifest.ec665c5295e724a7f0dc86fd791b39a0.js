047cd4c02a8b62107166ebdfb3f438b636299923
Kamil Jarmolowicz <kamiljarmol@protonmail.com>
2020-02-06 20:07:48 +0100

    Updates

---
+++ precache-manifest.ec665c5295e724a7f0dc86fd791b39a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1210e76c0db8976a817",
+    "url": "/app-rr/static/css/main.a1cf9350.chunk.css"
+  },
+  {
+    "revision": "f1210e76c0db8976a817",
+    "url": "/app-rr/static/js/main.1d324b14.chunk.js"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "6f10e4502a37126ddaf2",
+    "url": "/app-rr/static/js/2.92d84f41.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "9963db2dfdec14733cef73205d247a26",
+    "url": "/app-rr/index.html"
+  }
+];
